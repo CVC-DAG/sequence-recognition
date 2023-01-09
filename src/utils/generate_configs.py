2b961f8a7a47985bf371a878f61d848f8ba2f480
Pau Torras <pautorras1@hotmail.com>
2023-01-09 15:25:08 +0100

    small changes

---
+++ src/utils/generate_configs.py
@@ -0,0 +1,9 @@
+"""Generate configurations for grid/random search automatically from a 
+special configuration file."""
+
+from __future__ import annotations
+
+import json
+
+from pydantic import BaseModel
+
